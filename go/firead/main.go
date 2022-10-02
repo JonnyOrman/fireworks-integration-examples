8d58490ec8bf601e689ce1749add41510d1bae9c
Jonathan Orman <jonathanpcorman@gmail.com>
2022-10-02 12:04:16 +0100

    Add docker and go examples

---
+++ go/firead/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jonnyorman/firead"
+
+func main() {
+	firead.RunStringId()
+}
---
+++ go/firelete/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jonnyorman/firelete"
+
+func main() {
+	firelete.RunStringId()
+}
